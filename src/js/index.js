469cab5c2479aa0e9f7d6f55a4b729b87d2f1465
rachidbend <rachidbendaghor@gmail.com>
2020-08-02 09:09:14 +0100

    nav toggle working, ran build to test

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,49 @@
 import "./../sass/main.scss";
 
+
+// document.querySelector('.nav__mobile--open').classList.add('display');
+document.querySelector('.nav__mobile').addEventListener('click', e => {
+
+    // if there is a mobile class remove it 
+    // if there is no mobile class add it
+    const button = document.querySelector('.nav__mobile');
+    const nav = document.querySelector('.nav-mobile');
+    nav.classList.toggle('mobile-open');
+
+    if (nav.classList.contains('mobile-open')) {
+
+        document.querySelector('.nav__mobile--open').classList.remove('display');
+        document.querySelector('.nav__mobile--close').classList.add('display');
+    } 
+    else if (!nav.classList.contains('mobile-open')) {
+
+        // button.innerHTML = '<img class="nav__mobile--open" src="images/icon-hamburger.svg" alt=""></img>';
+        document.querySelector('.nav__mobile--close').classList.remove('display');
+        document.querySelector('.nav__mobile--open').classList.add('display');
+
+    } 
+    else {
+        console.log('there is an error');
+
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
